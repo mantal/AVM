f19d827ef874fae0a5293d9676d7083da2ac4878
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2017-01-26 23:32:41 +0100

    instructions a faires

---
+++ instructions.cpp
@@ -0,0 +1,22 @@
+template <class T>
+bool	push (T value, stack s)
+{
+
+}
+
+template <class T>
+bool	assert	(T value, stack s)
+{
+
+}
+
+bool	pop		(stack s) { }
+bool	dump	(stack s) { }
+bool	add		(stack s) { }
+bool	sub		(stack s) { }
+bool	mul		(stack s) { }
+
+bool	div		(stack s) { }
+bool	mod		(stack s) { }
+bool	print	(stack s) { }
+bool	exit	() { }
