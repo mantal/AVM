b8bd0f5d42a53a93ef508b2de423a13e25d161c2
mantal <mantal0@outlook.fr>
2017-01-31 18:56:54 +0100

    Remove boost usage in Operator

--- Operator.cpp
+++ Operator.cpp
@@ -1,15 +1,13 @@
 #include "Operator.hpp"
 #include <exception>
-#include <boost/format.hpp>
+#include <string>
 
 Operator::Operator(size_t param_n, size_t stack_param_n, char const *name) : _param_n(param_n), _stack_param_n(stack_param_n), name(name) { }
 
 void Operator::execute(VM &vm, std::vector<IOperand const*> const&) const
 {
 	if (vm.stack.size() < _stack_param_n)
-	{
-		throw new std::out_of_range((boost::format("Operator %1% need %2% in the stack\n") % name % vm.stack.size()).str().c_str());
-	}
+		throw new std::out_of_range("Operator " + name + " need " + std::to_string(vm.stack.size()) + " in the stack\n");
 }
 
 size_t Operator::getParamNumber(void) { return _param_n; }
