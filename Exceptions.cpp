40b3b4f30cc8169e9092189ce6ae610aa39a30a0
mantal <mantal0@outlook.fr>
2017-01-31 18:48:34 +0100

    Remove boost usage in Exceptions

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -1,8 +1,13 @@
 #include "Exceptions.hpp"
-#include <boost/format.hpp>
 
-UnknowOperatorException::UnknowOperatorException(std::string const& op) : std::runtime_error((boost::format("Unknow operator: %1%") % op).str()) {}
-UnknowOperandException::UnknowOperandException(std::string const& op) : std::runtime_error((boost::format("Unknow operand: %1%") % op).str()) {}//TODO pas boost
-SyntaxErrorException::SyntaxErrorException(std::string const& op) : std::runtime_error("Syntax error: " + op) {}
+UnknowOperatorException::UnknowOperatorException(std::string const& op)
+		: std::runtime_error("Unknow operator: " + op) {}
+
+UnknowOperandException::UnknowOperandException(std::string const& op)
+		: std::runtime_error("Unknow operand: " +  op) {}
+
+SyntaxErrorException::SyntaxErrorException(std::string const& op)
+		: std::runtime_error("Syntax error: " + op) {}
+
 OperandsMismatchException::OperandsMismatchException(std::string const& op, size_t expected, size_t found)
 		: std::runtime_error("Operator " + op + " expected " + std::to_string(expected) + " operands but " + std::to_string(found) + " were found") {}
