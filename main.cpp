152969ed34025d82d647f5f2d69619d5f0a06da1
mantal <mantal0@outlook.fr>
2017-02-01 16:33:52 +0100

    Run programs from files

--- main.cpp
+++ main.cpp
@@ -29,6 +29,31 @@ static std::vector<Expression const*> *fromStdin(void)
 	return expressions;
 }
 
+static std::vector<Expression const*> *fromFile(std::string const& fileName)
+{
+	auto expressions = new std::vector<Expression const*>;
+	Lexer lexer;
+	int i = 1;
+	bool error = false;
+	std::fstream stream(fileName);
+
+	for(std::string line; getline(stream, line); i++)
+	{
+		try
+		{
+			expressions->push_back(lexer.lex(line));
+		}
+		catch (std::exception const& e)
+		{
+			std::cerr << fileName << ": line " << i << ": " << e.what() << std::endl;
+			error = true;
+		}
+	}
+	if (error)
+		return nullptr;
+	return expressions;
+}
+
 int main(int argc, char **argv)
 {
 	VM vm;
@@ -36,6 +61,15 @@ int main(int argc, char **argv)
 	if (argc == 1)
 	{
 		vm.expressions = *fromStdin();
+		vm.run();
+		return 0;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		auto const e = fromFile(argv[i]);
+		if (e == nullptr)
+			continue ;
+		vm.expressions = *e;
 		vm.run();
 	}
 	return 0;
