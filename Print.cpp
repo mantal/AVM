cc2a9f060be796dfc134cf1c3e4322dcf71069da
mantal <mantal0@outlook.fr>
2017-02-08 16:07:53 +0100

    Fix Print

--- Print.cpp
+++ Print.cpp
@@ -10,9 +10,8 @@ void Print::execute(VM &vm, std::vector<IOperand const*> const& operands) const
 	Operator::execute(vm, operands);
 
 	auto &a = *vm.stack.front();
-	//vm.stack.pop_front();
 
 	if (a.getType() != eOperandType::Int8)
 		throw AssertionFailedException("an int8", "SOMEHTINGELSEFUCKC++");
-	std::cout << static_cast<char>(std::stoi(a.toString())) << std::endl;
+	std::cout << static_cast<char>(std::stoi(a.toString()));
 }
